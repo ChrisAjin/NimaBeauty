fe5d7348feb6361544edd4781835fa7dcc924f23
ChrisAjin <assassacorps@gmail.com>
2024-02-17 16:39:09 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { CustomerReviews,Footer,Hero,Services,Subscribe } from "./sections";
+import Nav from './components/Nav'
+const App = () => (
+	<main className="relative">
+		<Nav/>
+		<section className="x1:padding-1 wide:padding-r padding-b">
+			<Hero/>
+		</section>
+		<section className="padding-x py-10">
+			<Services/>
+		</section>
+		<section className="bg-pale-blue padding">
+			<CustomerReviews/>
+		</section>
+		<section className="padding-x sm:py-32 py-16 w-full">
+			<Subscribe/>
+		</section>
+		<section className="bg-black padding-y padding-t pb-8">
+			<Footer/>
+		</section>
+	</main>
+);
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+
+export {
+	Hero,
+	Footer,
+	Services,
+	Subscribe,
+	CustomerReviews
+}
